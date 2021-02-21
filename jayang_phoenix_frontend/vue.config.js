c965f2230349cb988b1dbddba717d74b3341419b
강준식 <kcu1436@naver.com>
2021-02-21 15:05:17 +0900

    - proxy & build path 세팅

--- jayang_phoenix_frontend/vue.config.js
+++ jayang_phoenix_frontend/vue.config.js
@@ -1,3 +1,22 @@
+const path = require("path");
+
 module.exports = {
-  transpileDependencies: ["vuetify"]
+    outputDir: path.resolve(__dirname, "../jayang_phoenix_backend/public"), //build 결과물 세팅
+    transpileDependencies: ["vuetify"],
+    //front-end Proxy Setting
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000'
+            }
+        }
+    },
+    configureWebpack: {
+        optimization: {
+            splitChunks: false
+        }
+    },
+    filenameHashing: false,
+    chainWebpack: config => config.optimization.minimize(false),
+    publicPath: './',
 };
