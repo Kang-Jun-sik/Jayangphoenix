ba5b3e734c4a06ffbd5169b9d3ed05339517633a
강준식 <kjs1436@douzone.com>
2021-03-05 16:21:30 +0900

    - vuetify 추가

--- jayang_frontend/src/main.js
+++ jayang_frontend/src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue'
 import router from './router'
 import axios from "axios"
 import "bootstrap/dist/css/bootstrap.css"
+import vuetify from './plugins/vuetify';
 
 const base = axios.create({
-    baseURL: "http://localhost:3000"
+    //baseURL: "http://localhost:3000"
     //baseURL: "http://10.105.1.234:3000"
 })
 
@@ -14,5 +15,6 @@ Vue.config.productionTip = false;
 
 new Vue({
     router,
-    render: h => h(App),
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
